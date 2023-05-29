1393b2887defb2d73d3f259254aa04bf6b3e8596
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-29 04:58:03 -0400

    modified

--- src/components/HeaderOne.vue
+++ src/components/HeaderOne.vue
@@ -2,7 +2,7 @@
 
 </script>
 <template>
-    <header class="w-full border-b px-10 py-3  sticky top-0 right-0 left-0 shadow-lg">
+    <header class="w-full border-b px-10 py-3  sticky top-0 right-0 left-0 shadow-lg z-10 bg-white">
         <div class=" font-Roboto text-2xl text-gray-700">What To-Do?</div>
     </header>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,6 +6,31 @@ import HeaderOne from '../components/HeaderOne.vue';
 <template>
     <HeaderOne/>
     <main class="w-full py-5 px-10 min-h-screen">
-        
+        <div class="flex flex-col gap-10 mt-5 mb-5">
+            <!-- the searching engine -->
+            <div>
+                <form @submit.prevent="handleSearchSubmission">
+                    <div class="relative">
+                        <i class="fa-solid fa-search absolute top-3 left-2 text-gray-500 z-5"></i>
+                        <input type="text" placeholder="Search User..." class="pl-8 pr-2 py-2 outline-none rounded-full border border-gray-300 shadow-sm focus:bg-gray-100 font-Roboto">
+                    </div>
+                </form>
+            </div>
+            <!-- the grid view -->
+            <div class="w-full flex flex-col px-5 ">
+                <!-- the add button -->
+                <div class="w-60 h-60 flex items-center justify-center bg-gray-100 border-gray-200 rounded-lg cursor-pointer transition transform hover:scale-105 hover:shadow-lg duration-200">
+                    <div>
+                        <!-- the image -->
+                        <div class="w-20 h-20 flex items-center justify-center bg-white border-gray-100 rounded-full"><i class="fa-solid fa-add text-2xl font-light text-gray-600"></i></div>
+                        <div class="font-light text-gray-500 text-sm pt-1 flex items-center justify-center">ADD NEW</div>
+                    </div>
+                </div>
+                <!--the grid container -->
+                <div>
+                    <!-- sub elements with user information -->
+                </div>
+            </div>
+        </div>
     </main>
 </template>
