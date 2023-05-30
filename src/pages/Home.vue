afaf1b6fd8b4e3088cb7a75268d415e11e955ca1
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-30 06:40:21 -0400

    some change

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -70,7 +70,7 @@ const handleCloseTheForm=()=>{
                 <!--the grid container -->
                 <div class="w-full grid grid-cols-4 gap-5 mt-5">
                     <!-- sub elements with user information -->
-                    <div v-for="(user,inded) in userData" :key="index" class="w-full border flex flex-col items-center justify-center p-2 cursor-pointer h-52 transform transition duration-200 hover:shadow-lg hover:scale-105 rounded-sm">
+                    <div v-for="(user,inded) in userData" :key="index" class="w-full border flex flex-col items-center justify-center p-2 cursor-pointer h-52 transform transition duration-200 hover:shadow-lg hover:scale-105 rounded-sm bg-gray-50">
                         <UserCard :user="user"/>
                     </div>
                 </div>
