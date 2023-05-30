9aecfb4dec36e6c46431f6f993058b6717d2195b
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-30 04:52:25 -0400

    some change

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { ref } from 'vue';
 
+const userName=ref('')
 const emit=defineEmits('close-the-form')
 
 
 const handleSubmission=()=>{
     
+}
+const emitClose=()=>{
+    userName.value=''
+    emit('close-the-form')
 }
 
 </script>
@@ -13,7 +19,7 @@ const handleSubmission=()=>{
 
     <Teleport to="body">
         <Transition name="create-user-form">
-            <div class="fixed top-0 bottom-0 left-0 right-0 z-20 bg-gray-500 opacity-50 flex items-center justify-center">
+            <div class="fixed top-0 bottom-0 left-0 right-0 z-20 bg-gray-500 bg-opacity-50 flex items-center justify-center">
                 <!-- the container -->
                 <div class="bg-white opacity-100 w-1/2 md:w-1/3 rounded-sm shadow-lg py-3 relative">
                     <!-- the title bar -->
@@ -23,7 +29,7 @@ const handleSubmission=()=>{
                             Creating User
                         </div>
                         <!-- the exit -->
-                        <div class="absolute -right-5 -top-5 text-2xl cursor-pointer"><i class="fa-solid fa-times text-black"></i></div>
+                        <div class="absolute -right-5 -top-5 text-2xl cursor-pointer" @click="emitClose"><i class="fa-solid fa-times text-black"></i></div>
                     </div>
                     <!-- the body part -->
                     <div class="flex items-center justify-center px-3 py-5">
@@ -36,13 +42,13 @@ const handleSubmission=()=>{
 
                             <!-- the form -->
                             <form @submit.prevent="handleSubmission" class="flex flex-col gap-3 py-4">
-                                <label>User Name:</label>
-                                <div>
-                                    <i class="fa-solid fa-user-circle"></i>
-                                    <input type="text" name="username" placeholder="User Name" >
+                                <label class="font-Roboto text-sm font-bold">User Name:</label>
+                                <div class="relative font-Roboto">
+                                    <i class="fa-solid fa-user-circle absolute top-2 left-2 text-gray-500" ></i>
+                                    <input type="text" name="username" placeholder="User Name" class="outline-none w-64 border border-gray-700 rounded-full pl-7 pr-2 py-1" v-model="userName" >
                                 </div>
                                 <div>
-                                    <input type="submit" name="create" value="Create">
+                                    <input type="submit" name="create" value="Create" class="outline-none w-64 border border-gray-700 text-center rounded-full py-1 px-2 text-white font-bold font-Roboto bg-black cursor-pointer">
                                 </div>
                             </form>
                         </div>
