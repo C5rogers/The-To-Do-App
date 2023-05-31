4e07b3d33ef9fd76bbb6fe2819114f2968140177
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-31 13:56:37 -0400

    some change

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -48,7 +48,9 @@ const handleCloseTheForm=()=>{
             <!-- the grid view -->
 
             <!-- the loading animation is here -->
-            <div v-if="loading">loadding...</div>
+            <div v-if="loading" class="w-full flex items-center justify-center h-96">
+                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><circle cx="4" cy="12" r="3" fill="currentColor"><animate id="svgSpinners3DotsBounce0" attributeName="cy" begin="0;svgSpinners3DotsBounce1.end+0.25s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="12" cy="12" r="3" fill="currentColor"><animate attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.1s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="20" cy="12" r="3" fill="currentColor"><animate id="svgSpinners3DotsBounce1" attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.2s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle></svg>
+            </div>
 
             <div class="w-full flex flex-col px-5 " v-else>
                 <!-- the add button -->
