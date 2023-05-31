3ec863ebba3aa24b06b9246ed2eec6daec47d949
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-31 12:58:39 -0400

    now it is configured with apollo client

---
+++ src/AppoloClient.js
@@ -0,0 +1,17 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core";
+
+const httpLink = createHttpLink({
+    uri: 'https://needed-bulldog-19.hasura.app/v1/graphql',
+    headers: {
+        "x-hasura-admin-secret": "V4RtkeZOJ49Ewk6yqfWBbqUbRiryQ1SxZMEVvn40fgHunM3Rb9TI61V22z4DYdkE"
+    }
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+})
+
+export default apolloClient
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import apolloClient from './AppoloClient'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
 
 const app = createApp(App);
+
+app.provide(DefaultApolloClient, apolloClient)
+
 app.use(router)
 app.mount('#app')
