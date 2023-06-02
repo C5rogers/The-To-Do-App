aa022e6a537319af788c0e1d8c9a3fd28a325b00
c5rogers <natenaelhaylemariam@gmail.com>
2023-06-02 12:59:51 -0400

    some change

--- src/components/ConfirmDelete.vue
+++ src/components/ConfirmDelete.vue
@@ -29,23 +29,26 @@ const handleConfirmation=(e)=>{
 
 <template>
     <!-- the confirmation -->
-    <div class="">
-        <!-- the title -->
-        <div>
-            {{ title }}
-        </div>
-        <!-- the message -->
-        <div>
-            <p>{{ message }}</p>
-        </div>
-        <!-- the form -->
-        <div>
-            <form @submit="handleConfirmation">
-                <div>
-                    <input type="submit" name="yes" value="Yes">
-                    <input type="submit" name="no" value="No">
-                </div>
-            </form>
+    <div class="fixed top-0 left-0 right-0 bottom-0 z-10 bg-gray-500 bg-opacity-50 flex items-center justify-center">
+        <!-- the whole container -->
+        <div class="flex flex-col font-Roboto w-fit p-5 gap-3 bg-red-700 text-white rounded-lg shadow-lg">
+            <!-- the title -->
+            <div class="text-2xl fotn-bold">
+                {{ title }}
+            </div>
+            <!-- the message -->
+            <div class="text-gray-100 font-light text-sm">
+                <p>{{ message }}</p>
+            </div>
+            <!-- the form -->
+            <div class="self-end mt-3">
+                <form @submit="handleConfirmation">
+                    <div class="flex gap-4">
+                        <input type="submit" name="yes" value="Yes" class="cursor-pointer bg-black  px-2  rounded-sm">
+                        <input type="submit" name="no" value="No" class="cursor-pointer bg-green-500 text-black px-2 rounded-sm">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 
